715900955601cfae84dcb7f8d88b3fe3eedf7825
madBennett <bennettmadison427@gmail.com>
2025-03-01 16:28:47 -0800

    Refactor Player Movement

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    //Movement
+    private InputAction moveAction;
+    [SerializeField] private PlayerInput playerInput;
     [SerializeField] private float speed = 5f;
     private Vector2 movement;
 
     public Rigidbody2D rigidBody;
 
+    private void Awake()
+    {
+        moveAction = playerInput.actions["Move"];
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +28,8 @@ public class PlayerBehavior : MonoBehaviour
     void Update()
     {
         //move the player based on user input
-        movement.Set(InputManager.movement.x, InputManager.movement.y);
+        movement = moveAction.ReadValue<Vector2>();
+        //movement.Set(InputManager.movement.x, InputManager.movement.y);
         rigidBody.velocity = movement * speed;
     }
 }
