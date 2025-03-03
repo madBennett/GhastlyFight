de8d94ed9cc5cd5a872af2b5f6159f42b4f63a5a
madBennett <bennettmadison427@gmail.com>
2025-03-02 21:28:02 -0800

    And Enemy Start

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    //for damage
+    private bool isVulenerable = true;
+
+    //health
+    public float curHealth;
+    public float maxHealth = 1000f;
+    public ValueBar healthBar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
