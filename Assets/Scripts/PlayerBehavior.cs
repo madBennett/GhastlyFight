ad6a1de744f908253d161b481081a1c290f86450
madBennett <bennettmadison427@gmail.com>
2025-03-07 12:30:18 -0800

    Add Player Wrapper for Multiplayer

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -36,6 +36,7 @@ public class PlayerBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         //set speed and dash time
         currSpeed = normSpeed;
         lastDashTime = Time.time;
