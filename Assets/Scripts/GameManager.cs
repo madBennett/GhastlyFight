419e3c0a9ead02784c7b489a6ff7a43f689d63ba
madBennett <bennettmadison427@gmail.com>
2025-03-14 09:30:07 -0700

    Revert Game Frame Counter

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,23 +16,21 @@ public class GameManager : NetworkBehaviour
 {
     public static GameStates gameState;
     public static bool isEnemyDead = false; //varible to alert players when the enemy is "killed"
-    [SerializeField] private NetworkVariable<int> testFrameCount = new NetworkVariable<int>(0);
-    public TMP_Text TestText;
 
     // Start is called before the first frame update
     void Start()
     {
         gameState = GameStates.GAME_PHASE1;
-        testFrameCount.OnValueChanged += CountChanged;
     }
 
     // Update is called once per frame
     void Update()
     {
-        testFrameCount.Value += 1;
+        
     }
+
     private void CountChanged(int previousValue, int newValue)
     {
-        TestText.text = "" + newValue;
+        
     }
 }
